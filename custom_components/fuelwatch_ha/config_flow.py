245a88581eecddd35ded16bca22b643b95d66631
jamiergrs <jamie_rogers@icloud.com>
2026-04-10 18:25:35 +0800

    Config Flow Tidy Up

--- custom_components/fuelwatch_ha/config_flow.py
+++ custom_components/fuelwatch_ha/config_flow.py
@@ -16,7 +16,6 @@ from .const import (
     DOMAIN,
     LOCATION_MODE_COORDINATES,
     LOCATION_MODE_HOME,
-    LOCATION_MODE_OPTIONS,
     LOCATION_MODE_ZONE,
 )
 
@@ -28,18 +27,37 @@ FUEL_TYPES = [
     "98RON",
 ]
 
+LOCATION_MODE_LABELS = {
+    LOCATION_MODE_HOME: "Home Location",
+    LOCATION_MODE_ZONE: "Home Assistant Zone",
+    LOCATION_MODE_COORDINATES: "Coordinates",
+}
+
 
 class FuelWatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    def __init__(self):
+        self._step_one_input = {}
+
     async def async_step_user(self, user_input=None):
         errors = {}
 
         if user_input is not None:
-            data, errors = self._validate_user_input(user_input)
+            data, errors = self._validate_step_one(user_input)
             if not errors:
-                return self.async_create_entry(
-                    title=data[CONF_NAME],
-                    data=data,
-                )
+                self._step_one_input = data
+
+                if data[CONF_LOCATION_MODE] == LOCATION_MODE_HOME:
+                    return self.async_create_entry(
+                        title=data[CONF_NAME],
+                        data=data,
+                    )
+
+                if data[CONF_LOCATION_MODE] == LOCATION_MODE_ZONE:
+                    return await self.async_step_zone()
+
+                return await self.async_step_coordinates()
 
         return self.async_show_form(
             step_id="user",
@@ -50,16 +68,76 @@ class FuelWatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     vol.Required(CONF_RADIUS, default=DEFAULT_RADIUS): int,
                     vol.Required(
                         CONF_LOCATION_MODE, default=LOCATION_MODE_HOME
-                    ): vol.In(LOCATION_MODE_OPTIONS),
-                    vol.Optional(CONF_ZONE_NAME, default=DEFAULT_ZONE_NAME): str,
-                    vol.Optional(CONF_LATITUDE, default=""): str,
-                    vol.Optional(CONF_LONGITUDE, default=""): str,
+                    ): vol.In(LOCATION_MODE_LABELS),
                 }
             ),
             errors=errors,
         )
 
-    def _validate_user_input(self, user_input):
+    async def async_step_zone(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            zone_name = user_input.get(CONF_ZONE_NAME, "").strip()
+            if not zone_name:
+                errors[CONF_ZONE_NAME] = "zone_required"
+            else:
+                data = {
+                    **self._step_one_input,
+                    CONF_ZONE_NAME: zone_name,
+                    CONF_LATITUDE: None,
+                    CONF_LONGITUDE: None,
+                }
+                return self.async_create_entry(
+                    title=data[CONF_NAME],
+                    data=data,
+                )
+
+        return self.async_show_form(
+            step_id="zone",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_ZONE_NAME, default=DEFAULT_ZONE_NAME): str,
+                }
+            ),
+            errors=errors,
+        )
+
+    async def async_step_coordinates(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            latitude = self._parse_coordinate(
+                user_input.get(CONF_LATITUDE, ""), CONF_LATITUDE, -90, 90, errors
+            )
+            longitude = self._parse_coordinate(
+                user_input.get(CONF_LONGITUDE, ""), CONF_LONGITUDE, -180, 180, errors
+            )
+
+            if not errors:
+                data = {
+                    **self._step_one_input,
+                    CONF_ZONE_NAME: DEFAULT_ZONE_NAME,
+                    CONF_LATITUDE: latitude,
+                    CONF_LONGITUDE: longitude,
+                }
+                return self.async_create_entry(
+                    title=data[CONF_NAME],
+                    data=data,
+                )
+
+        return self.async_show_form(
+            step_id="coordinates",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_LATITUDE): str,
+                    vol.Required(CONF_LONGITUDE): str,
+                }
+            ),
+            errors=errors,
+        )
+
+    def _validate_step_one(self, user_input):
         data = dict(user_input)
         errors = {}
 
@@ -70,27 +148,14 @@ class FuelWatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data[CONF_NAME] = name
 
         location_mode = data.get(CONF_LOCATION_MODE, LOCATION_MODE_HOME)
-        zone_name = data.get(CONF_ZONE_NAME, "").strip()
-        data[CONF_ZONE_NAME] = zone_name
-
-        if location_mode == LOCATION_MODE_ZONE and not zone_name:
-            errors[CONF_ZONE_NAME] = "zone_required"
-
-        if location_mode == LOCATION_MODE_COORDINATES:
-            latitude = self._parse_coordinate(
-                data.get(CONF_LATITUDE, ""), CONF_LATITUDE, -90, 90, errors
-            )
-            longitude = self._parse_coordinate(
-                data.get(CONF_LONGITUDE, ""), CONF_LONGITUDE, -180, 180, errors
-            )
-            data[CONF_LATITUDE] = latitude
-            data[CONF_LONGITUDE] = longitude
-        else:
-            data[CONF_LATITUDE] = None
-            data[CONF_LONGITUDE] = None
-
         if location_mode == LOCATION_MODE_HOME:
             data[CONF_ZONE_NAME] = DEFAULT_ZONE_NAME
+            data[CONF_LATITUDE] = None
+            data[CONF_LONGITUDE] = None
+        else:
+            data[CONF_ZONE_NAME] = None
+            data[CONF_LATITUDE] = None
+            data[CONF_LONGITUDE] = None
 
         return data, errors
 
